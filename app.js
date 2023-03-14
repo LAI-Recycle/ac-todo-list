c60a22df1abcc4a3773af23772d8f6a795a6bc87
LAI-Recycle <xfjioyhnplus@gmail.com>
2023-03-14 17:45:47 +0800

    feat: add handlebars

--- app.js
+++ app.js
@@ -2,14 +2,16 @@
 const express = require('express')
 const mongoose = require('mongoose') // 載入 mongoose
 
+const exphbs = require('express-handlebars')
+
 // 加入這段 code, 僅在非正式環境時, 使用 dotenv
 if (process.env.NODE_ENV !== 'production') {
   require('dotenv').config()
 }
 
 const app = express()
-mongoose.connect(process.env.MONGODB_URI , { useNewUrlParser: true , useUnifiedTopology: true }) // 設定連線到 mongoDB
 
+mongoose.connect(process.env.MONGODB_URI , { useNewUrlParser: true , useUnifiedTopology: true }) // 設定連線到 mongoDB
 
 // 取得資料庫連線狀態
 const db = mongoose.connection
@@ -22,8 +24,15 @@ db.once('open', () => {
   console.log('mongodb connected!')
 })
 
-app.get("/" , ( req,res) =>{
-  res.send("hello world")
+//主程式啟用 Handlebars
+app.engine('hbs', exphbs({ defaultLayout: 'main', extname: '.hbs' }))
+app.set('view engine', 'hbs')
+
+
+// 設定路由
+// Todo 首頁
+app.get('/', (req, res) => {
+  res.render('index')
 })
 
 app.listen( 3000 , ()=>{
