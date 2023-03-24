a1b1ac1b0080948f100cd7a63d7d96742ae6976d
LAI-Recycle <xfjioyhnplus@gmail.com>
2023-03-24 17:33:15 +0800

    feat: add create todo function

--- app.js
+++ app.js
@@ -2,6 +2,7 @@
 const express = require('express')
 const mongoose = require('mongoose') // 載入 mongoose
 
+const bodyParser = require('body-parser')// 引用 body-parser
 const exphbs = require('express-handlebars')
 
 const Todo = require("./models/todo")
@@ -30,6 +31,8 @@ db.once('open', () => {
 app.engine('hbs', exphbs({ defaultLayout: 'main', extname: '.hbs' }))
 app.set('view engine', 'hbs')
 
+// 用 app.use 規定每一筆請求都需要透過 body-parser 進行前置處理
+app.use(bodyParser.urlencoded({ extended: true }))
 
 // 設定路由
 // Todo 首頁
@@ -40,6 +43,26 @@ app.get('/', (req, res) => {
     .then(todos => res.render('index', { todos })) // 將資料傳給 index 樣板
     .catch(error => console.error(error)) // 錯誤處理
 })
+
+//新增一筆 To-do
+app.get('/todos/new', (req, res) => {
+  return res.render('new')
+})
+
+//Create 功能：資料庫新增資料
+app.post('/todos', (req, res) => {
+  const name = req.body.name       // 從 req.body 拿出表單裡的 name 資料
+  
+  // const todo = new Todo({ name })
+  // return todo.save() 
+  //   .then(() => res.redirect('/')) // 新增完成後導回首頁
+  //   .catch(error => console.log(error))
+
+  return Todo.create({ name })     // 存入資料庫
+    .then(() => res.redirect('/')) // 新增完成後導回首頁
+    .catch(error => console.log(error))
+})
+
 
 app.listen( 3000 , ()=>{
   console.log("app is running on http://localhost:3000.")
