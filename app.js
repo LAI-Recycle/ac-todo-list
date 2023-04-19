14e067f5400d68039efc8464074114a8b4d75199
LAI-Recycle <xfjioyhnplus@gmail.com>
2023-04-20 01:44:32 +0800

    refactor: modify mongoose config

--- app.js
+++ app.js
@@ -1,36 +1,15 @@
 // app.js
 const express = require('express')
-const mongoose = require('mongoose') // 載入 mongoose
- 
 const bodyParser = require('body-parser')// 引用 body-parser
 const exphbs = require('express-handlebars')
 // 載入 method-override
 const methodOverride = require('method-override')
 
-const Todo = require("./models/todo")
-
 // 引用路由器
 const routes = require('./routes')
- 
-// 加入這段 code, 僅在非正式環境時, 使用 dotenv
-if (process.env.NODE_ENV !== 'production') {
-  require('dotenv').config()
-}
+require('./config/mongoose')
 
 const app = express()
-
-mongoose.connect(process.env.MONGODB_URI , { useNewUrlParser: true , useUnifiedTopology: true }) // 設定連線到 mongoDB
-
-// 取得資料庫連線狀態
-const db = mongoose.connection
-// 連線異常
-db.on('error', () => {
-  console.log('mongodb error!')
-})
-// 連線成功
-db.once('open', () => {
-  console.log('mongodb connected!')
-})
 
 //主程式啟用 Handlebars
 app.engine('hbs', exphbs({ defaultLayout: 'main', extname: '.hbs' }))
